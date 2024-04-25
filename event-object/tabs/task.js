c07cf412bf0aa975a154131e7cb1066ba4c38bae
Your Name <katy2606@list.ru>
2024-04-25 14:33:16 +0500

    last2

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,14 +1,13 @@
 let arrTabs = Array.from(document.getElementsByClassName('tab'))
 let arrContents = Array.from(document.getElementsByClassName('tab__content'))
-
 arrTabs.forEach((el) => el.addEventListener('click',function (event){
-        if(el.classList.contains('tab_active')){
-            console.log(arrTabs.findIndex(el))
-                arrContents[arrTabs.findIndex(el)].classList.remove('tab__content_active')
-            el.classList.remove('tab_active')
-            // }
-        }
+    if(arrTabs.findIndex(el => el.className.includes('tab_active'))!=-1){
+        arrContents[arrTabs.findIndex(el => el.className.includes('tab_active'))].classList.remove('tab__content_active')
+        arrTabs[arrTabs.findIndex(el => el.className.includes('tab_active'))].classList.remove('tab_active')
+    }else{
+        return;
+    }
     event.target.classList.add('tab_active')
-    arrContents[arrTabs.findIndex(event.target)].classList.add('tab__content_active')
+    arrContents[arrTabs.findIndex(el =>el == event.target)].classList.add('tab__content_active')
     })
 )
